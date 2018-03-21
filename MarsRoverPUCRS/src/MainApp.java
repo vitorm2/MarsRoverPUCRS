fe0d712f8271d3145a41313c3e7c2d226609b9ce
guilherme.deconto <guilherme.deconto@acad.pucrs.br>
2018-03-21 20:43:01 -0300

    Tratando exceções no método verificaGrade

--- MarsRoverPUCRS/src/MainApp.java
+++ MarsRoverPUCRS/src/MainApp.java
@@ -3,136 +3,143 @@ import java.util.Scanner;
 
 public class MainApp {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-		ArrayList<Rover> listaRovers =  new ArrayList<>();
-		
-		
-		// Auxiliares booleanas para controlar o menu
-		boolean aux_menu = true; 
-		boolean aux_menu2 = true;
-		
-		Scanner in = new Scanner(System.in);
-		Scanner in2 = new Scanner(System.in);
+        ArrayList<Rover> listaRovers = new ArrayList<>();
 
-		String gradeInformada = "";
 
-		System.out.print("Digite as dimensoes da grade: ");
+        // Auxiliares booleanas para controlar o menu
+        boolean aux_menu = true;
+        boolean aux_menu2;
 
-		gradeInformada = in.nextLine();
+        Scanner in = new Scanner(System.in);
+        Scanner in2 = new Scanner(System.in);
 
-		verificaGrade(gradeInformada);
-		
-		while(aux_menu) {
-			
-			aux_menu2 = true;
+        String gradeInformada;
 
-			String p = "";
-			
-			System.out.print("\n========= Configurando Rover =========\n\n");
-			System.out.print("Digite a posicao inicial do Rover: ");
-	
-			p = in2.nextLine();
+        System.out.print("Digite as dimensoes da grade: ");
 
-			Rover r1 = new Rover(verificaPosicaoInicial(p));
-			listaRovers.add(r1);
-	
-			String c = "";
-			
-			System.out.print("Digite os comandos para movimentar o Rover: ");
-			c = in2.nextLine();
-	
-			r1.movimentaRover(c);
-			
-			int escolha = 0;
-			
-			while(aux_menu2) {
-				
-				System.out.println("\n\nEscolha um opção: ");
-				System.out.println("1 - Criar outro Rover.");
-				System.out.println("2 - Listar todos Rovers.");
-				System.out.println("3 - Fechar.");
-				escolha = in.nextInt();
-				
-				
-				switch(escolha) {
-				case 1:{
-					aux_menu2 = false;
-					break;}
-				case 2:{ 
-					for(Rover r: listaRovers) {
-					System.out.println(r);
-				}	
-					break;}
-				case 3:{
-				aux_menu = false; 
-				aux_menu2 = false; 
-					break;
-				}
-				default:{
-					break;
-					}
-				}
-			}
-		}
-		 
-	}
-	
-	public static String verificaPosicaoInicial(String p) {
-		
-		String t = "";
-		String x1 = "";
+        gradeInformada = in.nextLine();
 
-		String y1 = "";
-		String d1 = "";
+        verificaGrade(gradeInformada);
 
-		
-		for (int i = 0; i < p.length(); i++) {
-       	 t=""+p.charAt(i);
-       	if(t.contentEquals(" ")) {
-				for(int j=i+1; j< p.length(); j++) {
-					t=""+p.charAt(j);
-					if(t.contentEquals(" ")) {
-						for (int k=j+1; k< p.length();k++) {
-						d1=d1+p.charAt(k);	
-							
-						}
-						break;
-					}
-					
-					y1=y1+p.charAt(j);
+        while (aux_menu) {
 
-				}
-				break;
-			}
-       	 x1=x1+p.charAt(i);
-			}
+            aux_menu2 = true;
 
-		return x1+y1+d1;
-		
-	}
 
-	public static String verificaGrade(String gradeInformada) {
-		
-		String x = "";
+            String p = "";
 
-		String y = "";
-		String t = "";
-		
-		for (int i = 0; i < gradeInformada.length(); i++) {
-			t = "" + gradeInformada.charAt(i);
-			if (t.contentEquals(" ")) {
-				for (int j = i + 1; j < gradeInformada.length(); j++) {
-					y = y + gradeInformada.charAt(j);
+            System.out.print("\n========= Configurando Rover =========\n\n");
+            System.out.print("Digite a posicao inicial do Rover: ");
 
-				}
-				break;
-			}
-			x = x + gradeInformada.charAt(i);
+            p = in2.nextLine();
 
-		}
-		
-		return t;
+            Rover r1 = new Rover(verificaPosicaoInicial(p));
+            listaRovers.add(r1);
 
-	}
+            String c = "";
+
+            System.out.print("Digite os comandos para movimentar o Rover: ");
+            c = in2.nextLine();
+
+            r1.movimentaRover(c);
+
+            int escolha = 0;
+
+            while (aux_menu2) {
+
+                System.out.println("\n\nEscolha um opção: ");
+                System.out.println("1 - Criar outro Rover.");
+                System.out.println("2 - Listar todos Rovers.");
+                System.out.println("3 - Fechar.");
+                escolha = in.nextInt();
+
+
+                switch (escolha) {
+                    case 1: {
+                        aux_menu2 = false;
+                        break;
+                    }
+                    case 2: {
+                        for (Rover r : listaRovers) {
+                            System.out.println(r);
+                        }
+                        break;
+                    }
+                    case 3: {
+                        aux_menu = false;
+                        aux_menu2 = false;
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+
+    public static String verificaPosicaoInicial(String p) {
+
+        String t = "";
+        String x1 = "";
+
+        String y1 = "";
+        String d1 = "";
+
+
+        for (int i = 0; i < p.length(); i++) {
+            t = "" + p.charAt(i);
+            if (t.contentEquals(" ")) {
+                for (int j = i + 1; j < p.length(); j++) {
+                    t = "" + p.charAt(j);
+                    if (t.contentEquals(" ")) {
+                        for (int k = j + 1; k < p.length(); k++) {
+                            d1 = d1 + p.charAt(k);
+
+                        }
+                        break;
+                    }
+
+                    y1 = y1 + p.charAt(j);
+
+                }
+                break;
+            }
+            x1 = x1 + p.charAt(i);
+        }
+
+        return x1 + y1 + d1;
+
+    }
+
+    public static String verificaGrade(String gradeInformada) throws Exception {
+
+        String x = "";
+
+        String y = "";
+        String t = "";
+        try {
+            if (Integer.parseInt(gradeInformada.replaceAll(" ","")) > 0) {
+                for (int i = 0; i < gradeInformada.length(); i++) {
+                    t = "" + gradeInformada.charAt(i);
+                    if (t.contentEquals(" ")) {
+                        for (int j = i + 1; j < gradeInformada.length(); j++) {
+                            y = y + gradeInformada.charAt(j);
+
+                        }
+                        break;
+                    }
+                    x = x + gradeInformada.charAt(i);
+
+                }
+            }
+
+        }catch (Exception e ){
+            throw new Exception("Formato da grade incorreta. As dimensões da grade só podem ser constituídas de números.");
+        }
+        return t;
+    }
 }
